9564d5533ea59f340d34d4da0a12072de22980ab
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-14 17:11:32 +0300

    + config.AuditingConfig + domain.DateAudit + domain.RoleName + domain.Role + domain.User

---
+++ backend/src/main/java/com/mcfly/poll/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.mcfly.poll.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
