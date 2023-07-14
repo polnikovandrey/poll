a03fa5aaf3ca6995155acb18ff47ec24485289b6
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-14 14:04:19 +0300

    Initial commit. + Poll backend

---
+++ backend/src/test/java/com/mcfly/poll/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcfly.poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
