a99274ae0ebcd4f9863767884defa146f509c480
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-15 18:25:55 +0300

    + flyway db init script +- properties

--- backend/src/main/resources/db.migration/V1__init_database.sql
+++
@@ -1,7 +0,0 @@
-# TODO
-# drop table if exists order_header;
-# create table order_header
-# (
-#   id BIGINT not null auto_increment primary key,
-#   customer_name varchar(255)
-# ) engine = InnoDB;
---
+++ backend/src/main/resources/db/migration/V1__init_database.sql
@@ -0,0 +1,26 @@
+drop table if exists roles;
+create table roles
+(
+    id   bigint not null auto_increment primary key,
+    name tinyint unique
+) engine = InnoDB;
+drop table if exists users;
+create table users
+(
+    id         bigint       not null auto_increment primary key,
+    username   varchar(15)  not null unique,
+    password   varchar(100) not null,
+    name       varchar(40)  not null,
+    email      varchar(40)  not null unique,
+    created_at datetime(6)  not null,
+    updated_at datetime(6)  not null
+) engine = InnoDB;
+drop table if exists user_roles;
+create table user_roles
+(
+    role_id bigint not null,
+    user_id bigint not null,
+    primary key (role_id, user_id),
+    constraint fk_user_roles_roles foreign key (role_id) references roles (id),
+    constraint fk_user_roles_users foreign key (user_id) references users (id)
+) engine = InnoDB;
