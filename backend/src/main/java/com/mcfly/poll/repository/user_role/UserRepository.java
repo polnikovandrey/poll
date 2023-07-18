11b5e2c3f63c22e511844e3260acaa09070c642c
Andrey Polnikov <polnikov.andrey@gmail.com>
2023-07-18 18:00:01 +0300

    + UserRoleRepositoryTest ...2

--- backend/src/main/java/com/mcfly/poll/repository/user_role/UserRepository.java
+++ backend/src/main/java/com/mcfly/poll/repository/user_role/UserRepository.java
@@ -8,14 +8,13 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsernameOrEmail(String username, String email);
-
-    List<User> findByIdIn(List<Long> userIds);
-
-    Optional<User> findByUsername(String username);
-
     Boolean existsByUsername(String username);
 
     Boolean existsByEmail(String email);
 
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsername(String username);
+
+    List<User> findByIdIn(List<Long> userIds);
 }
--- backend/src/test/java/com/mcfly/poll/repository/UserRoleRepositoryTest.java
+++ backend/src/test/java/com/mcfly/poll/repository/UserRoleRepositoryTest.java
@@ -6,6 +6,7 @@ import com.mcfly.poll.domain.user_role.User;
 import com.mcfly.poll.exception.AppException;
 import com.mcfly.poll.repository.user_role.RoleRepository;
 import com.mcfly.poll.repository.user_role.UserRepository;
+import jakarta.validation.ConstraintViolationException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +14,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
@@ -34,7 +38,28 @@ public class UserRoleRepositoryTest {
     }
 
     @Test
-    void testFindByUsernameOrEmail() {
+    void testPersistUserWithNotValidEmailFails() {
+        final User user = createRandomUser();
+        user.setEmail("not valid email");
+        assertThrows(ConstraintViolationException.class, () -> userRepository.saveAndFlush(user));
+    }
+
+    @Test
+    void testExistsUserByUsername() {
+        final User user = persistRandomUser();
+        final Boolean existsByUsername = userRepository.existsByUsername(user.getUsername());
+        assertTrue(existsByUsername);
+    }
+
+    @Test
+    void testExistsUserByEmail() {
+        final User user = persistRandomUser();
+        final Boolean existsByEmail = userRepository.existsByEmail(user.getEmail());
+        assertTrue(existsByEmail);
+    }
+
+    @Test
+    void testFindUserByUsernameOrEmail() {
         final User user = persistRandomUser();
         final Optional<User> userByUsername = userRepository.findByUsernameOrEmail(user.getUsername(), null);
         assertTrue(userByUsername.isPresent());
@@ -43,26 +68,26 @@ public class UserRoleRepositoryTest {
     }
 
     @Test
-    void testFindByIdIn() {
-        // TODO List<User> findByIdIn(List<Long> userIds);
+    void testFindUserByUsername() {
+        final User user = persistRandomUser();
+        final Optional<User> userByUsername = userRepository.findByUsername(user.getUsername());
+        assertTrue(userByUsername.isPresent());
     }
 
     @Test
-    void testFindByUsername() {
-        // TODO Optional<User> findByUsername(String username);
-    }
-
-    @Test
-    void testExistsByUsername() {
-        // TODO Boolean existsByUsername(String username);
-    }
-
-    @Test
-    void testExistsByEmail() {
-        // TODO Boolean existsByEmail(String email);
+    void testFindUserByIdIn() {
+        final User user1 = persistRandomUser();
+        final User user2 = persistRandomUser();
+        final List<User> foundUsers = userRepository.findByIdIn(List.of(user1.getId(), user2.getId()));
+        assertThat(foundUsers).isNotNull().isNotEmpty().size().isEqualTo(2);
     }
 
     private User persistRandomUser() {
+        final User user = createRandomUser();
+        return userRepository.saveAndFlush(user);
+    }
+
+    private User createRandomUser() {
         final String name = RandomStringUtils.randomAlphabetic(5);
         final String username = RandomStringUtils.randomAlphabetic(5);
         final String email = RandomStringUtils.randomAlphabetic(5) + "@gmail.com";
@@ -72,17 +97,6 @@ public class UserRoleRepositoryTest {
         user.setUpdatedAt(Instant.now());
         final Role userRole = roleRepository.findByName(RoleName.ROLE_USER).orElseThrow(() -> new AppException("User Role not set."));
         user.setRoles(Collections.singleton(userRole));
-        userRepository.saveAndFlush(user);
         return user;
     }
-
-
-    /*
-
-    assertThrows(EmptyResultDataAccessException.class, () -> bookRepository.readBookByTitle("No such title"));
-
-    RandomStringUtils.randomAlphabetic(51)
-
-     */
-
 }
